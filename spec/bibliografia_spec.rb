e5655f4b8f6bd201d2498f2b021dbae8a01e5de9
alu0100502107 <alu0100502107@ull.edu.es>
2015-11-11 22:20:05 +0000

    Añadidos libro2, libro3, libro4 y libro5

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -4,8 +4,8 @@ describe Bibliografia do
   
   context "Bibliografia" do
       
-  before :each do
-      @libro = Bibliografia::Referencia.new(
+  before :all do
+      @libro1 = Bibliografia::Referencia.new(
         ["Dave Thomas", "Andy Hunt", "Chad Fowler"], 
         "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide",
         "(The Facets of Ruby)",
@@ -14,66 +14,102 @@ describe Bibliografia do
 	    "(July 7, 2013)",
 	    ["968-1937785499", "1937785491"]
 	)
-   end
+	  @libro2 = Bibliografia::Referencia.new(
+        ["Scott Chacon"], 
+         "Pro Git 2009th Edition",
+        "Pro",
+	    "Apress",
+	     "2009 edition" ,
+	    "August 27, 2009",
+	    ["968-1430218333", "1430218339"]
+	)    
+	  @libro3 = Bibliografia::Referencia.new(
+         ["David Flanagan", "Yukihiro Matsumoto"], 
+         "The Ruby Programming Language",
+        "Serie",
+	    "O’Reilly Media",
+	     "1 edition" ,
+	    "February 4, 2008",
+	    ["0596516177", "978-0596516178"]
+	)    
+	  @libro4 = Bibliografia::Referencia.new(
+         ["David Chelimsky", "Dave Astels", " Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"], 
+         "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends",
+         "The Facets of Ruby",
+	    "Pragmatic Bookshelf",
+	     "1 edition" ,
+	    "December 25, 2010",
+	    ["1934356379", "978-1934356371"]
+	)
+	@libro5 = Bibliografia::Referencia.new(
+         ["Richard E. Silverman"], 
+         "Git Pocket Guide",
+         "Serie",
+	    "O’Reilly Media",
+	     "1 edition" ,
+	    "August 2, 2013",
+	    ["1449325866", "978-1449325862"]
+	)
+	end
    
    it "deben de existir uno o más autores" do
-     expect(@libro.autores).not_to be_empty 
+     expect(@libro1.autores).not_to be_empty 
    end
    
    it "debe de existir un título" do
-     expect(@libro.titulo).not_to be_nil
+     expect(@libro1.titulo).not_to be_nil
    end
    
    it "debe de existir una serie" do
-     expect(@libro.serie).not_to be_nil
+     expect(@libro1.serie).not_to be_nil
    end
    
    it "debe existir una editorial" do
-     expect(@libro.editorial).not_to be_nil
+     expect(@libro1.editorial).not_to be_nil
    end
 
    it "debe existir un número de edición" do
-     expect(@libro.num_edicion).not_to be_nil
+     expect(@libro1.num_edicion).not_to be_nil
    end
    
    it "debe existir una fecha de publicación" do
-     expect(@libro.fecha_publicacion).not_to be_nil
+     expect(@libro1.fecha_publicacion).not_to be_nil
    end
    
    it "debe existir uno o más números ISBN" do
-     expect(@libro.num_isbns).not_to be_empty 
+     expect(@libro1.num_isbns).not_to be_empty 
    end
    
    it "debe existir un método para obtener el listado de autores" do
-     expect(@libro.print_autor).to eq("Dave Thomas, Andy Hunt, Chad Fowler")
+     expect(@libro1.print_autor).to eq("Dave Thomas, Andy Hunt, Chad Fowler")
    end
    
    it "Existe método para obtener el titulo" do
-     expect(@libro.titulo).to eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")
+     expect(@libro1.titulo).to eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")
    end
    
    it "Existe un método para obtener la serie" do
-     expect(@libro.serie).to eq("(The Facets of Ruby)") 
+     expect(@libro1.serie).to eq("(The Facets of Ruby)") 
    end
    
    it "Existe un método que devuelve la editorial" do
-     expect(@libro.editorial).to eq("Pragmatic Bookshelf")     
+     expect(@libro1.editorial).to eq("Pragmatic Bookshelf")     
    end
    
    it "Existe un método para obtener el número de edición" do
-     expect(@libro.num_edicion).to eq("4 edition") 
+     expect(@libro1.num_edicion).to eq("4 edition") 
    end
    
    it "Existe un método para obtener la fecha de publicación" do
-     expect(@libro.fecha_publicacion).to eq("(July 7, 2013)") 
+     expect(@libro1.fecha_publicacion).to eq("(July 7, 2013)") 
    end
    
    it "Existe un método para obtener el listado ISBN" do
-     expect(@libro.print_isbn).to eq("ISBN-13: 968-1937785499\nISBN-10: 1937785491\n")
+     expect(@libro1.print_isbn).to eq("ISBN-13: 968-1937785499\nISBN-10: 1937785491\n")
    end
    
    it "Existe un método para obtener la referencia formateada" do
-    expect(@libro.to_s).to eq("Dave Thomas, Andy Hunt, Chad Fowler\nProgramming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide\n(The Facets of Ruby)\nPragmatic Bookshelf; 4 edition (July 7, 2013)\nISBN-13: 968-1937785499\nISBN-10: 1937785491\n")
+    expect(@libro1.to_s).to eq("Dave Thomas, Andy Hunt, Chad Fowler\nProgramming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide\n(The Facets of Ruby)\nPragmatic Bookshelf; 4 edition (July 7, 2013)\nISBN-13: 968-1937785499\nISBN-10: 1937785491\n")
    end
    
    end
