3051d9c74d33393da239f8b07c22664dfb0bc590
alu0100502107 <alu0100502107@ull.edu.es>
2015-12-18 11:08:35 +0000

    Adaptada la clase Articulo_periodico a DSL

--- lib/bibliografia/bibliografia.rb
+++ lib/bibliografia/bibliografia.rb
@@ -205,121 +205,66 @@ module Bibliografia
       salida << "#{num_paginas.join(', ')} "
       return salida
     end
-  end 
-
-
-  # class Articulo_libro < Referencia
-  #   # Getter + Setter
-  #   attr_accessor :ediciones, :num_ediciones, :volumenes, :titulos2, :num_paginas, :lugares_publicacion, :editoriales
-    
-  #   # Constructor
-  #   def initialize(titulo ,&bloque)
-  #     self.titulo = titulo
-  #     self.autores = []
-  #     self.fechas_publicacion = []   
-  #     self.titulos2 = []
-  #     self.num_paginas = []
-  #     self.ediciones = []
-  #     self.volumenes = []
-  #     self.lugares_publicacion = []
-  #     self.editoriales = []
-    
-  #     instance_eval &bloque if block_given?
-  #   end
-    
-  #   def autor(nombre, options = {})
-  #     autor = nombre
-  #     autores << autor
-  #   end
-    
-  #   def fecha_publicacion(formato, options = {})
-  #     fecha_publicacion = formato
-  #     fechas_publicacion <<  fecha_publicacion
-  #   end
-    
-  #   def titulo2(nombre, options = {})
-  #     titulo2 = nombre
-  #     titulos2 << titulo2
-  #   end
-     
-  #   def edition(name, options = {})
-  #         edition = name
-  #         edicion << edition
-  #   end
-     
-    
-     
-     
-     
-  #   def volume(name, options = {})
-  #         volume = name
-  #         volumen << volume
-  #   end
-     
-  #   def place(name, options = {})
-  #         place = name
-  #         lpublicacion << place
-  #   end
-     
-  #   def editor(name, options = {})
-  #         editor = name
-  #         edito << editor
-  #   end
-     
-  #   def to_s()
-          
-  #         out = "#{titulo}"
-  #         out << " #{autor.join(', ')} "
-  #         out << "(#{fecha.join(', ')}) "
-  #         out << " #{titulob.join(', ')} "
-  #         out << " #{pags.join(', ')} "
-  #         out << "(#{edicion.join(', ')}) "
-  #         out << "(#{volumen.join(', ')}) "
-  #         out << "#{lpublicacion.join(', ')} "
-  #         out << "#{edito.join(', ')} "
-          
-  #         out
-  #   end
-     
-  # end
+  end # Articulo_libro
   
-  # class Articulo_revista < Referencia
-  #   # Getter + Setter
-  #   attr_accessor :nombre_revista, :lugar_publicacion, :idioma
+  class Articulo_periodico < Referencia
+    # Getters + Setters 
+    attr_accessor :lugares_publicacion, :nombres_periodico, :num_paginas
+     
+    # Constructor 
+    def initialize(titulo, &bloque)
+      self.titulo = titulo
+      self.autores = []
+      self.fechas_publicacion = []
+      self.lugares_publicacion = []
+      self.nombres_periodico = []
+      self.num_paginas = []
     
-  #   # Constructor
-  #   def initialize(*parametros, nombre_revista, lugar_publicacion, idioma)
-  #       super(*parametros)
-  #       @nombre_revista = nombre_revista
-  #       @lugar_publicacion = lugar_publicacion
-  #       @idioma = idioma
-  #   end
-  # end
+      instance_eval &bloque if block_given?
+    end
+    
+    # Introduce un string con el autor de un artículo de un periodico
+    def autor(nombre, options = {})
+      autor = nombre
+      autores << autor
+    end
+    
+    # Introduce un string con la fecha de publicación de un artículo de un periodico
+    def fecha_publicacion(ano, options = {})
+      fecha_publicacion = ano
+      fechas_publicacion << fecha_publicacion
+    end
+    
+    # Introduce un string con el lugar de publicación de un artículo de un periodico  
+    def lugar_publicacion(pais, options = {})
+      lugar_publicacion = pais
+      lugares_publicacion << lugar_publicacion
+    end
+    
+    # Introduce un string con el nombre del periodico de un artículo de un periodico    
+    def nombre_periodico(nombre, options = {})
+      nombre_periodico = nombre
+      nombres_periodico << nombre_periodico
+    end
+    
+    # Introduce un string con el número de página de un artículo de un periodico  
+    def num_pagina(numero, options = {})
+      num_pagina = numero
+      num_paginas << num_pagina
+    end
+    
+    # Para método puts 
+    def to_s()
+      salida = "#{titulo},"
+      salida << " #{autores.join(', ')}, "
+      salida << "(#{fechas_publicacion.join(', ')}), "
+      salida << "#{lugares_publicacion.join(', ')}, "
+      salida << "#{nombres_periodico.join(', ')}, "
+      salida << "#{num_paginas.join(', ')} "
+      return salida
+    end
+  end     
   
-  # class Articulo_periodico < Referencia
-  #   # Getter + Setter
-  #   attr_accessor :nombre_periodico, :num_paginas
-    
-  #   # Constructor
-  #   def initialize(*parametros, nombre_periodico, num_paginas)
-  #       super(*parametros)
-  #       @nombre_periodico = nombre_periodico
-  #       @num_paginas = num_paginas
-  #   end
-  # end
-  
-  # class Documento_electronico < Referencia
-  #   # Getter + Setter
-  #   attr_accessor :medio, :lugar_publicacion, :url
-    
-  #   # Constructor
-  #   def initialize(*parametros, medio, lugar_publicacion, url)
-  #       super(*parametros)
-  #       @medio = medio
-  #       @lugar_publicacion = lugar_publicacion
-  #       @url = url
-  #   end
-  # end
 end
 
 
